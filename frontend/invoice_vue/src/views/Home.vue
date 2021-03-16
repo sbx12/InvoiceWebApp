c406cbcfee414942abf9d772ac8c6d16336efc4f
sbx12 <sbx018@gmail.com>
2021-03-16 18:24:25 -0400

    Django and Vue Setup

---
+++ frontend/invoice_vue/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1 class="title">Welcome to Invoice</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
