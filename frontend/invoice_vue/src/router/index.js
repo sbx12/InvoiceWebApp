293930605df452ba4edecfb8d33282ee43c6a8b2
sbx12 <sbx018@gmail.com>
2021-03-16 23:59:48 -0400

    Added Bulma Toast Messages

--- frontend/invoice_vue/src/router/index.js
+++ frontend/invoice_vue/src/router/index.js
@@ -6,6 +6,7 @@ import Clients from '../views/dashboard/Clients.vue'
 import Client from '../views/dashboard/Client.vue'
 import AddClient from '../views/dashboard/AddClient.vue'
 import EditClient from '../views/dashboard/EditClient.vue'
+import EditTeam from '../views/dashboard/EditTeam.vue'
 import SignUp from '../views/SignUp.vue'
 import Login from '../views/Login.vue'
 
@@ -47,6 +48,14 @@ const routes = [
     path: '/dashboard/my-account',
     name: 'MyAccount',
     component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/my-account/edit-team',
+    name: 'EditTeam',
+    component: EditTeam,
     meta: {
       requireLogin: true
     }
--- frontend/invoice_vue/src/views/dashboard/EditClient.vue
+++ frontend/invoice_vue/src/views/dashboard/EditClient.vue
@@ -78,6 +78,7 @@
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
 
 export default {
     name: 'EditClient',
@@ -105,6 +106,14 @@ export default {
             axios
                 .patch(`/api/v1/clients/${clientID}/`, this.client)
                 .then(response => {
+                    toast({
+                        message: 'The changes was saved',
+                        type: 'is-success',
+                        dismissible: true,
+                        pauseOnHover: true,
+                        duration: 2000,
+                        position: 'bottom-right',
+                    })
                     this.$router.push('/dashboard/clients')
                 })
                 .catch(error => {
