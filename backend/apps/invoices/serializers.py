9fb1a1b8d73c9a406957ee6667c3ec6806a1edd4
sbx12 <sbx018@gmail.com>
2021-03-17 14:22:33 -0400

    Added Item to Rest and few changes to inovices

--- backend/apps/invoices/serializers.py
+++ backend/apps/invoices/serializers.py
@@ -5,7 +5,16 @@ from .models import Invoice, Item
 
 
 class InvoiceSerialzier(serializers.ModelSerializer):
+    client = serializers.StringRelatedField()
+    
     class Meta:
         model = Invoice
         read_only_fields = ['team', 'created_at', 'created_by', 'modified_by', 'modified_at']
+        fields = '__all__'
+        
+
+class ItemSerialzier(serializers.ModelSerializer):    
+    class Meta:
+        model = Item
+        read_only_fields = ['invoice']
         fields = '__all__'
--- backend/apps/invoices/views.py
+++ backend/apps/invoices/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
-from .serializers import InvoiceSerialzier
+from .serializers import InvoiceSerialzier, ItemSerialzier
 
 # Models
 from .models import Invoice, Item
@@ -24,3 +24,12 @@ class InvoiceViewSet(viewsets.ModelViewSet):
             raise PermissionDenied('Wrong object Owner')
         
         serializer.save()
+        
+
+class ItemViewSet(viewsets.ModelViewSet):
+    serializer_class = ItemSerialzier
+    queryset = Item.objects.all()
+    
+    def get_queryset(self):
+        invoice_id = self.request.GET.get('invoice_id', 0)
+        return self.queryset.filter(invoice__id=invoice_id)
